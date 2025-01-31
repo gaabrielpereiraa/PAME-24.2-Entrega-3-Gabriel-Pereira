5c847ad8326b001dd6aae2415b522bef585b698c
gaabrielpereiraa <gabriel.20241@poli.ufrj.br>
2025-01-31 00:22:40 -0300

    Mudanca no banco de dados

--- src/app/models/lolipops_list.ts
+++ src/app/models/lolipops_list.ts
@@ -1,8 +1,7 @@
 import { Lolipop } from './lolipop'
 
 export  const lolipops = [
-    new Lolipop('1', 'Pirulito de Cereja', 'Cereja', '5,00', 'Pirulito de cereja', 'Pirulito de cereja muito foda', 'https://m.media-amazon.com/images/I/51chR9jy2VS.jpg', 'Cereja', true),
-    new Lolipop('2', 'Pirulito de Uva', 'Uva', '6,00', 'Pirulito de uva', 'Pirulito de uva muito gostoso', 'https://docemalu.vtexassets.com/arquivos/ids/5358260-800-auto?v=638687437976570000&width=800&height=auto&aspect=true', 'Uva', true),
-    new Lolipop('3', 'Pirulito de Maçã Verde', 'Maçã Verde', '7,00', 'Pirulito de maçã verde', 'Sabor refrescante e único', 'https://docemalu.vtexassets.com/arquivos/ids/5342206-800-auto?v=638421777059700000&width=800&height=auto&aspect=true', 'Maçã Verde', true),
-    new Lolipop('4', 'Pirulito de Morango', 'Morango', '5,50', 'Pirulito de morango', 'Doce e irresistível', 'https://images-americanas.b2w.io/produtos/19582399/imagens/pirulito-big-big-morango-50-unidades/19582397_1_xlarge.jpg', 'Morango', true)
+    new Lolipop('1', 'Pirulito de Cereja', 'Cereja', '4,00', 'Pirulito de cereja', 'Pirulito de cereja', 'https://m.media-amazon.com/images/I/51chR9jy2VS.jpg', 'Xilitol, Aroma artificial de cereja, Ácido cítrico, Extrato de spirulina, Extrato natural de cereja, Suco de beterraba, Isomalte, Ácido ascórbico, sorbato de potássio', true),
+    new Lolipop('2', 'Pirulito de Melância', 'Melância', '4,00', 'Pirulito de uva', 'Pirulito de uva', 'https://jadoces.com.br/wp-content/uploads/2023/09/rot-pirulito-blong-28-melancia_97348952358879a6e38a1fa4da424975.png', 'Xilitol, Aroma artificial de melância, Ácido cítrico, Extrato de spirulina, Extrato natural de melância, Suco de beterraba, Isomalte, Ácido ascórbico, sorbato de potássio', true),
+    new Lolipop('3', 'Pirulito de Morango', 'Morango', '4,00', 'Pirulito de morango', 'Pirulito de morango', 'https://images-americanas.b2w.io/produtos/19582399/imagens/pirulito-big-big-morango-50-unidades/19582397_1_xlarge.jpg', 'Xilitol, Aroma artificial de morango, Ácido cítrico, Extrato de spirulina, Extrato natural de morango, Suco de beterraba, Isomalte, Ácido ascórbico, sorbato de potássio', true)
   ];
